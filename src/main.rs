1ebbb23652f4847cd0584236218eaad173dcf30d
Darion Spaargaren <djspaargaren@outlook.com>
2023-02-22 18:00:39 +0100

    feat: :sparkles: Add Yew Router, add new pages

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Debug, Clone, Copy, PartialEq, Routable)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/projects")]
+    Projects,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! {
+            <Home />
+        },
+        Route::Projects => html! {
+            <Projects />
+        },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
 
 #[function_component(App)]
 fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} /> // <- must be child of <BrowserRouter>
+        </BrowserRouter>
+    }
+}
+
+#[function_component(Home)]
+fn home() -> Html {
     html! {
         <>
             <p class="bio">{ "Hi, I'm Steve!"} <br/>
@@ -12,6 +49,11 @@ fn app() -> Html {
     }
 }
 
-fn main() {
-    yew::Renderer::<App>::new().render();
+#[function_component(Projects)]
+fn projects() -> Html {
+    html! {
+        <>
+            <h1 class="title">{"Projects"}</h1>
+        </>
+    }
 }

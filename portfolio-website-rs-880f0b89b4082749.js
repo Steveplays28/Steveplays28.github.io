841b7c6752e3da02233bb425d83a59d2ac3387b5
Steveplays28 <Steveplays28@users.noreply.github.com>
2023-06-25 15:55:44 +0000

    deploy: 6d9b977d385b6f2c8b54d591f1bc7424b68be999

--- portfolio-website-rs-880f0b89b4082749.js
+++ portfolio-website-rs-880f0b89b4082749.js
@@ -853,7 +853,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('portfolio-website-rs-875adc311dec3789_bg.wasm', import.meta.url);
+        input = new URL('portfolio-website-rs-880f0b89b4082749_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
